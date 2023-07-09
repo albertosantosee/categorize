045dc88758b1806c144a395376a49106eb6caf1f
santoscaj <santosc.aj@gmail.com>
2023-07-09 02:19:18 -0300

    better, not quite yet there though

--- natural.js
+++ natural.js
@@ -4,33 +4,18 @@ const categories = require('./categories');
 function categorizeItem(description) {
   const classifier = new natural.BayesClassifier();
   for (const [category, keywords] of Object.entries(categories)) {
-    for (const keyword of keywords) {
-      classifier.addDocument(keyword, category);
-    }
+    classifier.addDocument(keywords.join(' '), category);
   }
   classifier.train();
 
-  const itemProbabilities = classifier.getClassifications(description);
-  itemProbabilities.sort((a, b) => b.value - a.value); // Sort by probability in descending order
+  const matchedCategory = classifier.classify(description);
+  const isMiscellaneous = Object.values(categories).every((keywords) => !keywords.includes(matchedCategory));
 
-  const topCategory = itemProbabilities[0].label; // Get the top category
-  const threshold = 0.1; // Adjust the threshold as needed
-
-  if (topCategory === 'none' || itemProbabilities[0].value < threshold) {
-    // Check if the description contains any toy-related keywords
-    const toyKeywords = categories.toys;
-    const hasToyKeyword = toyKeywords.some((keyword) => description.toLowerCase().includes(keyword));
-    if (hasToyKeyword) {
-      return 'toys'; // Return "toys" category if the description contains toy-related keywords
-    }
-    return 'miscellaneous'; // Return "miscellaneous" category for unrecognized or low probability inputs
-  }
-
-  return topCategory;
+  return isMiscellaneous ? 'miscellaneous' : matchedCategory;
 }
 
 // Example usage
-const description = "Wooden Montessori Toys for Toddlers, Carrot Harvest, Shapes Sorting &Matching, Stacking...";
+const description = "12V 5A Power Supply Led Strip Light 120V to 12V Transformer Input with 5.5x2.1mm 60W 12V AC DC";
 const itemCategory = categorizeItem(description);
 console.log(itemCategory);
 
